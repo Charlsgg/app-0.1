bee29a7c4d2877ace698c9a96b3c8510b1c470f1
Charlsgg <mcklienxx@gmail.com>
2026-03-18 11:11:36 +0800

    updated vite config.ts

--- CCIS-MIRROR/vite.config.ts
+++ CCIS-MIRROR/vite.config.ts
@@ -3,11 +3,19 @@ import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            refresh: true,
-        }),
-        vue(),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.ts'],
+      refresh: true,
+    }),
+    vue(),
+  ],
+  server: {
+    host: '0.0.0.0',      // Listen on all network interfaces
+    port: 5173,           // Default Vite port
+    hmr: {
+      host: '192.168.1.15', // Your LAN IP
+      port: 5173,
+    },
+  },
 })
